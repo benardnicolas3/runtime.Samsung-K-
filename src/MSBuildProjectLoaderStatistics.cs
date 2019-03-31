c7adcc6543d7c1d0135d6c21b990f847512f8072
nico leputois <NicoLeputois@hotmail.com>
2019-03-31 14:18:55 +0200

    Commit

---
+++ src/MSBuildProjectLoaderStatistics.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace SlnGen.Build.Tasks.Internal
+{
+    /// <summary>
+    /// Represents statistics of operations performed by the <see cref="MSBuildProjectLoader"/> class.
+    /// </summary>
+    internal sealed class MSBuildProjectLoaderStatistics
+    {
+        private readonly ConcurrentDictionary<string, TimeSpan> _projectLoadTimes = new ConcurrentDictionary<string, TimeSpan>();
+
+        public IEnumerable<KeyValuePair<string, TimeSpan>> ProjectLoadTimes => _projectLoadTimes;
+
+        internal bool TryAddProjectLoadTime(string path, TimeSpan timeSpan)
+        {
+            return _projectLoadTimes.TryAdd(path, timeSpan);
+        }
+    }
+}
